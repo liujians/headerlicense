8c1efd51b4a6856466aba902daf4d3edffef1874
刘剑 <jian.liu@rootcloud.com>
2021-07-21 11:40:28 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	
+	const license:string = vscode.workspace.getConfiguration().get('headerLicense.license')||"";
+	const template:string = license;
+// 	const temp = `/*
+// 	* Licensed Materials - Property of ROOTCLOUD
+// 	* THIS MODULE IS "RESTRICTED MATERIALS OF ROOTCLOUD"
+// 	* (c) Copyright ROOTCLOUD Inc. 2021 All Rights Reserved
+// 	*
+// 	* The source code for this program is not published or
+// 	* otherwise divested of its trade secrets
+// 	*/
+// `
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "headerlicense" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('headerlicense.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('welcome headerLicense!');
+	// });
+
+	vscode.workspace.onWillSaveTextDocument(file=>{
+		if (!file.document.isDirty) return
+		const editor = vscode.window.activeTextEditor;
+		editor?.edit(editBuilder=>{
+			console.log("====")
+			console.log(file.document.getText());
+			console.log("====")
+			console.log(template);
+			console.log("====")
+			if(!file.document.getText().includes(template)){
+				editBuilder.insert(new vscode.Position(0, 0), template+"\n")
+			}
+		})
+		vscode.window.showInformationMessage('保存了');
+	})
+
+	vscode.workspace.onDidCreateFiles((file) => {
+		const filePath = file.files[0].fsPath
+		const openPath = vscode.Uri.file(filePath)
+		vscode.workspace.openTextDocument(openPath).then((doc) => {
+			vscode.window.showTextDocument(doc).then(() => {
+					const editor = vscode.window.activeTextEditor // 每次运行选中文件
+					editor?.edit(editBuilder=>{
+							editBuilder.insert(new vscode.Position(0, 0), template+"\n")
+					})
+			})
+		})
+	})
+
+	// context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
