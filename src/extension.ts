3cf82352ab09efb5a4f0413aaa34f52d2857a820
刘剑 <jian.liu@rootcloud.com>
2021-07-21 15:40:30 +0800

    fix version

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,6 @@
  * The source code for this program is not published or
  * otherwise divested of its trade secrets
  */
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
